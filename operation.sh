bcb7bb5e1f706fc3cf43421c62b5761533f301d0
Rashmi Ghosh <rashmighoshofficial963@gmail.com>
2020-06-03 12:16:37 +0530

    ADDED: Computation of operation 3 being added

--- operation.sh
+++ operation.sh
@@ -1,4 +1,3 @@
-#!/bin/bash -x
 echo "Welcome to Arithmetic operation"
 echo "Reading Three inputs"
 read -p "Enter the first Number = " a
@@ -6,6 +5,8 @@ read -p "Enter the second Number = " b
 read -p "Enter the third Number = " c
 
 op1=$(( $a + $b * $c ))
-
+echo $op1
 op2=$(( $a % $b + $c ))
-
+echo $op2
+op3=`echo "scale=2;$c + $a / $b" | bc`
+echo $op3
