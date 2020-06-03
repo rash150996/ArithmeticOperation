067ed674fa947ac70117cb64a915e5d0b0d1c4d0
Rashmi Ghosh <rashmighoshofficial963@gmail.com>
2020-06-03 11:49:01 +0530

    ADDED: Functionality to read three inputs

--- operation.sh
+++ operation.sh
@@ -1,2 +1,7 @@
+#!/bin/bash -x
 echo "Welcome to Arithmetic operation"
+echo "Reading Three inputs"
+read -p "Enter the first Number = " a
+read -p "Enter the second Number = " b
+read -p "Enter the third Number = " c
 
