41fbc53a8eee195cceaf48aef622eee06f236672
Rashmi Ghosh <rashmighoshofficial963@gmail.com>
2020-06-03 12:33:23 +0530

    MODIFIED: All the operation values stored in Dictionary

--- operation.sh
+++ operation.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -x
 echo "Welcome to Arithmetic operation"
 echo "Reading Three inputs"
 read -p "Enter the first Number = " a
@@ -5,10 +6,38 @@ read -p "Enter the second Number = " b
 read -p "Enter the third Number = " c
 
 op1=$(( $a + $b * $c ))
-echo $op1
 op2=$(( $a % $b + $c ))
-echo $op2
 op3=`echo "scale=2;$c + $a / $b" | bc`
-echo $op3
 op4=$(( $a * $b + $c ))
-echo $op4
+
+declare -A Arithmetic
+i=1
+while (( i<=4 ))
+do
+	case $i in
+			1)
+				op1=$(( $a + $b * $c ))
+				Arithmetic["$i"]=$op1
+				;;
+			2)
+				op2=$(( $a % $b + $c ))
+            Arithmetic["$i"]=$op2
+            ;;
+			3)
+            op3=`echo "scale=2;$c + $a / $b" | bc`
+            Arithmetic["$i"]=$op3
+            ;;
+			4)
+            op4=$(( $a * $b + $c ))
+            Arithmetic["$i"]=$op4
+            ;;
+			*)
+				echo "Invalid"
+				;;
+	esac
+	i=`expr $i + 1`
+done
+
+echo ${Arithmetic[@]}
+
+
